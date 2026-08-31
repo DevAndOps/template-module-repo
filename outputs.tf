a50ac35cad82559548802e6b348e912252b1893f
hamza-aus <hamza.chd12354@gmail.com>
2026-09-01 00:51:39 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@
 #   description = "REPLACE."
 #   value       = aws_xxx.this.id
 # }
+
+output "tags" {
+  description = "Tags this module applies to every taggable resource it creates."
+  value       = local.tags
+}
